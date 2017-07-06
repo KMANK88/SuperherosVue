cb5f99a678188ac6e90797c07fb2db292cde767b
Karsten Pease <karstenpease88@outlook.com>
2017-07-06 10:51:28 -0600

    updated

--- superHerosVue/server.js
+++ superHerosVue/server.js
@@ -1,10 +1,43 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var Superhero = require('./models/Superhero');
+var mongoose = require('mongoose');
 var app = express();
 var port = 3000;
+
+
+mongoose.connect('mongodb://localhost/superheroes');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+ app.get('/', function(req, res) {
+  Superhero.find(function(err, superheroes) {
+    if (err) throw err;
+    res.json({ data : superheroes, message : "Heroes successfully retrieved!" })
+  })
+});
+
+
+app.get("/:_id", function(req,res){
+  Superhero.findById(req.params._id,)
+})
+
+app.post('/', function(req, res) {
+  var superhero = new Superhero();
+  superhero.name = req.body.name;
+  superhero.superpower = req.body.superpower;
+
+  superhero.save().then(function(superhero) {
+    res.send(superhero);
+  }, function(err) {
+    res.send(err)
+  })
+});
+
 
 
 var server = app.listen(port,function(){
   console.log("Listening on port", port)
 })
-//jja
